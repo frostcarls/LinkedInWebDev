99ccd320dfa6e531ea6d9c2408802803686f9974
Ashfaaq <ashfaaq24@gmail.com>
2018-05-11 12:46:55 +0530

    Find a job added

--- PHP/find_a_job.php
+++ PHP/find_a_job.php
@@ -6,17 +6,20 @@
 		<link rel = "stylesheet" href = "../css/form_login.css">
 		<link rel = "stylesheet" href = "../css/side_menu.css">
 		<style type="text/css">
-            a {text-decoration: none}
+			a {text-decoration: none}
 
-            div#forgot {
-                /* Modal Content/Box */
-    			font-family: Courier , serif;
-    			font-size: 20px;
-    			background-color: #fefefe;
-    			margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
-    			border: 1px solid transparent;
-    			width: 80%; /* Could be more or less, depending on screen size */
-            }
+			/* Modal Content/Box */
+			.modal-content {
+				padding: 10px;
+				font-family: Courier , serif;
+				font-size: 20px;
+			    background-color: #fefefe;
+			    margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+			    border: 1px solid transparent;
+				border-radius: 0.3cm;
+			    width: 80%; /* Could be more or less, depending on screen size */
+			}
+
 		</style>
 		<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 
@@ -35,15 +38,7 @@
 			<a href="../pages/submit_resume.html">Submit Your Resume</a>
 			<a href="../pages/contact.html">Contacts</a>
 		</div>
-
-        <div id = "forgot">
-            Enter the email address :
-            <form method = "POST" action = "forgot.php">
-                <input type = "text" name = "mail" size = "40" placeholder = "Email address">
-                <button type = "submit">Confirm</button>
-            </form>
-        </div>
-    </body>
+	</body>
 </html>
 
 <?php
@@ -54,30 +49,27 @@ $dbname = "webdev";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-$email = $_POST['mail'];
-
-$sql= "SELECT * FROM sign_up";
+$sql= "SELECT * FROM find_job";
 $result = $conn->query($sql);
 
-$flag = 1;
+$key = $_POST["company"];
 
 if($result->num_rows > 0)
 {
     while($row = $result->fetch_assoc())
     {
-        if($row['email'] == $email)
+        if($row['id_no'] == $key)
         {
-            echo "<div id = \"forgot\">Your password is : " . $row['password'] . "</div>";
+            echo "<div id = \"modal\">";
+            echo "<div class = \"modal-content\">";
+            echo "<h2>Job Offerings :</h2><hr><br>";
+                	 echo "Company Name : " . $row['com_name'] . "<br>Region : " .
+    				$row['region'] . '<br>Job offered : ' . $row['job'] . '<br><br><br>';
+                    echo "</div></div>";
+            break;
         }
     }
 }
 
+mysqli_close($conn);
 ?>
-
-<html>
-<hr>
-<footer>
-    <p>All rights reserved &copy 2018 Nasha Technologies</p>
-    <p>Contact information: <a href="mailto:ashfaaqomg@gmail.com">mailtonashatech@gmail.com</a>.</p>
-</footer>
-</html>
--- PHP/submit_resume.php
+++ PHP/submit_resume.php
@@ -1,6 +1,5 @@
 <html>
 <?php
-session_start();
 $servername = "localhost";
 $username = "root";
 $password = "root";
@@ -11,21 +10,27 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if(!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-//echo "Connection successful !";
+echo "Connection successful !";
 
-$gcollege = $_POST['gcollege'];
-$gyear = $_POST['gyear'];
-$exp = $_POST['exp'];
-$hobby = $_POST['hobbies'];
-$lang = $_POST['lang'];
-$email = $_POST['email'];
-
-$_SESSION['uname'] = $email;
+$gcollege = $_POST["gcollege"];
+$gyear = $_POST["gyear"];
+$exp = $_POST["exp"];
+$hobby = $_POST["hobbies"];
+$lang = $_POST["lang"];
+$email = $_POST["email"];
 
 $sql = "INSERT INTO resume (gcollege, gyear, experience, hobby, languages, email)
 VALUES ('$gcollege', '$gyear', '$exp', '$hobby', '$lang', '$email')";
 
+if(mysqli_query($conn, $sql)) {
+    echo "Resume completed !!";
+} else {
+    echo "Error: " . $sql . mysqli_error($conn);
+}
+
 mysqli_close($conn);
+session_start();
+$_SESSION['uname'] = $email;
 
 header('location: /webdev/PHP/resume.php');
 ?>
